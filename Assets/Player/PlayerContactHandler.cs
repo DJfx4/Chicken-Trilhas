2c51cbabb2252ded930cbc5a086e667937a4d4d8
DJfx4 <demetriojsilva4@gmail.com>
2025-03-08 01:51:59 -0400

    A4 - segunda fase e sons

--- Assets/Player/PlayerContactHandler.cs
+++ Assets/Player/PlayerContactHandler.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+
+    public Image itemImage;
+
+    public PlayerAudioController audioController;
+
+    bool canWinlevel = false;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -18,6 +25,21 @@ public class Player : MonoBehaviour
         if (collision.gameObject.CompareTag("Item"))
         { 
             Debug.Log("PLAYER PEGOU O ITEM!");
+            Destroy(collision.gameObject);
+            itemImage.color = Color.white;
+            canWinlevel = true;
+            audioController.PlayGetItem();
+        }
+
+        if (collision.gameObject.CompareTag("FinalPoint"))
+        {
+            if (canWinlevel)
+            {
+                Debug.Log("PLAYER GANHOU O LEVEL!!!");
+            } else
+            {
+                Debug.Log("PLAYER NÃO GANHOU O LEVEL!!!");
+            }
         }
     }
 
